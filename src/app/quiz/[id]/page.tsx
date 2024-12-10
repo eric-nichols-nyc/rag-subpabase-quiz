eb8434ad054b177a3e8c8ad6e28a3c22147ed0f5
Eric Nichols <ebn646@hotmail.com>
2024-12-10 10:24:38 -0500

    docs: readme update

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -1,10 +1,8 @@
-import { Header } from "@/components/header"
 import QuizGallery from "@/components/quiz-gallery"
 
 export default async function QuizPage({ params }: { params: { id: string } }) {
   return (
     <div className="min-h-screen bg-background">
-      <Header />
       <main className="container mx-auto px-4 py-8">
         <QuizGallery id={params.id}/>
       </main>
